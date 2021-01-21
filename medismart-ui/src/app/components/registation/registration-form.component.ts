d96e1ce974e3164f6e8c6b56a8fc094a9d3f3da7
Aishat-Akinyemi <52966744+Aishat-Akinyemi@users.noreply.github.com>
2021-01-21 15:37:49 +0100

    update registration form component

--- medismart-ui/src/app/components/registation/registration-form.component.ts
+++ medismart-ui/src/app/components/registation/registration-form.component.ts
@@ -1,4 +1,8 @@
+import { PatientService } from './../../services/patient.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-registration-form-component',
@@ -6,10 +10,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration-form.component.css']
 })
 export class RegistrationFormComponent implements OnInit {
+  gender = ['Female', 'Male'];
+  registrationForm: FormGroup;
 
-  constructor() { }
+  constructor( private fb: FormBuilder, private snackBar: MatSnackBar, private patientService: PatientService, public matDialog: MatDialog) { }
 
   ngOnInit(): void {
+    this.registrationForm = this.fb.group({
+      name: ['', Validators.required],
+      phoneNumber: [null, Validators.pattern('[0-9]*')],
+      emailAddress: [''],
+      age: [Date],
+      gender: [''],
+      weight: [null],
+      height: [null],
+      address: [''],
+      underlyingCondition: ['']
+    })
+  }
+
+  viewRecords(){
+
+  }
+
+  formatObject(){
+    this.registrationForm.get('age').setValue(parseInt(this.registrationForm.get('age').value, 10));
+    this.registrationForm.get('gender').setValue(parseInt(this.registrationForm.get('gender').value, 10));
+  }
+
+  submitForm(){ 
+    this.formatObject();
+    this.patientService.addNewPatient(this.registrationForm.value).subscribe();
+    this.snackBar.open('Patient Record Submitted');
+    this.registrationForm.reset();
   }
 
 }
