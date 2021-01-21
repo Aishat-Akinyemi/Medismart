fbc8e3318480229bbb76d5c2dca0a8be39e44b3b
Aishat-Akinyemi <52966744+Aishat-Akinyemi@users.noreply.github.com>
2021-01-21 21:33:20 +0100

    update registration form component

--- medismart-ui/src/app/components/record/record.component.ts
+++ medismart-ui/src/app/components/record/record.component.ts
@@ -1,5 +1,7 @@
+import { IPatient } from './../../models/patient';
 import { PatientService } from './../../services/patient.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTable } from '@angular/material/table';
 
 @Component({
   selector: 'app-record-component',
@@ -7,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./record.component.css']
 })
 export class RecordComponent implements OnInit {
-
+  patients: IPatient[];
+  @ViewChild(MatTable) table: MatTable<any>;
+  errorMessage: any;
+  displayedColumns: string[] = ['id', 'name', 'phoneNumber','email', 'gender', 'age', 'bmi', 'address', 'bmi','underlyingCondition', 'edit', 'delete' ]
   constructor(private patientService: PatientService) { }
 
   ngOnInit(): void {
+    this.patientService.getPatients().subscribe({
+      next: (patients)  => {
+        this.patients = patients;
+      },
+      error: (err) => {this.errorMessage = err; }
+    })
   }
+ 
+  deletePatient(patientDel: IPatient){
+   
 
+  //   this.patientService.deletePatient(patientDel).subscribe(
+  //     () => {
+  //       this.patients = this.patients.filter((patient) => patient.patientID !== patientDel.patientID);
+  //       this.table.renderRows();
+  //     }
+  //   )
+  // }
+
+  this.patientService.deletePat(patientDel.patientID).subscribe
+  (
+        () => {
+          this.patients = this.patients.filter((patient) => patient.patientID !== patientDel.patientID);
+          this.table.renderRows();
+        }
+      )}
 }
