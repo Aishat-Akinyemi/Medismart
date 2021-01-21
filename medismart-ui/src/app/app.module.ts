19d2e6ee27519d09a3b0abfadfe4e2a345404618
Aishat-Akinyemi <52966744+Aishat-Akinyemi@users.noreply.github.com>
2021-01-21 13:51:21 +0100

    Generate UI components

--- medismart-ui/src/app/app.module.ts
+++ medismart-ui/src/app/app.module.ts
@@ -4,15 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from "./material/material.module";
+import { NavbarComponentComponent } from './components/navbar-component.component';
+import { RegistrationFormComponentComponent } from './components/registation/registration-form-component.component';
+import { RecordComponentComponent } from './components/record/record-component.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponentComponent,
+    RegistrationFormComponentComponent,
+    RecordComponentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ medismart-ui/src/app/components/record/record-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-record-component',
+  templateUrl: './record-component.component.html',
+  styleUrls: ['./record-component.component.css']
+})
+export class RecordComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
